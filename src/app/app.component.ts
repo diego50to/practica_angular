ee4978eb2a90d68a13bc7ce600fa2f074bcc5b5a
diego50to <diegofernandosoto@gmail.com>
2022-10-03 21:04:56 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pfa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Practica_Final_Angular';
+}
